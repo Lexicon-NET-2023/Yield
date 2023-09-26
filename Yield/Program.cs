fb043841ea56442bef127cc76d74a45dfa7f615e
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-26 13:47:32 +0200

    First

--- Yield/Program.cs
+++ Yield/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var numbers = GetNumbers();
+IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+while (enumerator.MoveNext())
+{
+    int item = enumerator.Current;
+    Console.WriteLine(item);
+}
+
+foreach (var number in GetNumbers())
+{
+    Console.WriteLine(number);
+}
+
+IEnumerable<int> GetNumbers()
+{
+    var num =  new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+    foreach (var number in num)
+    {
+        yield return number;
+    }
+}
